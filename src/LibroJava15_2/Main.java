9c6902e27e4c129638e282e9dfba4a332d127497
EsperanzaE <esperanza.berrocal@iesnervion.es>
2025-01-12 21:56:43 +0100

    Commit inicial

---
+++ src/LibroJava15_2/Main.java
@@ -0,0 +1,12 @@
+package LibroJava15_2;
+
+public class Main {
+   public static void main(String[] args) {
+       String verde = "\033[32m";
+
+       String miNombre =" Esperanza Berrocal", inicio="Mi nombre es";
+       System.out.println(inicio + verde+ miNombre);
+
+
+    }
+}
---
+++ src/LibroJava6_1/Main.java
@@ -0,0 +1,10 @@
+package LibroJava6_1;
+//jhjh
+public class Main {
+   public static void main(String[] args) {
+       String verde = "\033[32m";
+       System.out.println("Mi nombre es" + verde+ " Esperanza Berrocal");
+
+
+    }
+}
